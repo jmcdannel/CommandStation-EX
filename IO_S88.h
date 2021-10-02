01acd756dfb0fe692b06d957385964a035b4f8d3
Neil McKechnie <neilmck999@gmail.com>
2021-10-02 16:36:19 +0100

    Update IO_S88.h
    Allow the time allowed for reading a bit to be configured in the device creation.  Also, restrict the number of bits read per loop entry to keep loop time low (particularly important when the bit-time is large).

--- IO_S88.h
+++ IO_S88.h
@@ -18,43 +18,46 @@
  */
 
 /*
- * The S88 Bus is essentially a shift register consisting of one or more 
+ * The S88 Bus is essentially a shift register consisting of one or more
  * S88 modules, connected one to another in one long chain.  The register
  * is read by the following steps:
- *   1) The LOAD/PS line goes to HIGH, then the CLK line is pulsed HIGH.  This tells
- *      the registers to acquire data from the latched parallel inputs.
- *   2) With LOAD/PS still high, the RESET signal is pulsed HIGH, which clears 
- *      the parallel input upstream latches.
- *   3) With LOAD/PS LOW, the shift is performed by pulsing the CLK line high.  On each
- *      pulse, the data in the shift register is presented, bit by bit, to the
- *      DATA-IN line.
- * 
+ *   1) The LOAD/PS line goes to HIGH, then the CLK line is pulsed HIGH.  This
+ * tells the registers to acquire data from the latched parallel inputs. 2) With
+ * LOAD/PS still high, the RESET signal is pulsed HIGH, which clears the
+ * parallel input upstream latches. 3) With LOAD/PS LOW, the shift is performed
+ * by pulsing the CLK line high.  On each pulse, the data in the shift register
+ * is presented, bit by bit, to the DATA-IN line.
+ *
  * Example configuration in mySetup.cpp:
  *    #include "IO_S88.h"
  *    void mySetup() {
- *      IO_S88::create(2500, 16, 40,41,42,43);
+ *      IO_S88::create(2500, 16, 40,41,42,43, 10);
  *    }
- * 
- * This creates an S88 bus instance with 16 inputs (VPINs 2500-2515).  The 
- * LOAD pin is 40, RESET is 41, CLK is 42 and DATA is 43.  These four pins 
+ *
+ * This creates an S88 bus instance with 16 inputs (VPINs 2500-2515).
+ * The LOAD pin is 40, RESET is 41, CLK is 42 and DATA is 43.  These four pins
  * must be local GPIO pins (not on an I/O expander).
- * 
+ * The last parameter (bitTime=10) means that at least 10us is allowed for
+ * reading each bit in the shift register.
+ *
  * Depending on the S88 modules being used and the cabling, the timing of the
- * interface may have to be adjusted.  There are three parameters below that 
- * affect this. 
- * 
- * _pulseDelayTime determines how much extra time is allowed 
- * between pulse transitions, to allow for propagation times.
- * 
- * _bitsReadPerLoopEntry determines the number of bits read in each loop entry;
- * if the pulse delay time is increased, the loop entry will take longer, so
- * bitsReadPerLoopEntry should be reduced to compensate.
- * 
- * _acquireCycleTime determines the minimum time between successive acquire cycles
- * of the inputs on the S88 bus.  Bear in mind that the Sensor code includes 
- * anti-bounce logic which means that fleeting state changes may be ignored,
- * so reducing the acquireCycleTime may not have the desired effect.
- * 
+ * interface may have to be adjusted.  This is done by the bit time parameter.
+ * The original S88 concept was based on 4014 shift register devices, which are
+ * easily capable of moderate speed transfers and would cope with a bit time
+ * around 2us.  However, commercial S88 devices appear to be extremely slow in
+ * comparison and may need the order of tens or hundreds of microseconds.  The
+ * symptom of a bit time that is too small is that, when you activate one input to
+ * the S88 module, the command station sees something other than the correct
+ * activation; e.g. no activations, multiple activations, the wrong pin
+ * activating etc.
+ *
+ * _acquireCycleTime determines the minimum time between successive acquire
+ * cycles of the inputs on the S88 bus.  Bear in mind that the Sensor code
+ * includes anti-bounce logic which means that fleeting state changes may be
+ * ignored, so reducing the acquireCycleTime may not have the desired effect.
+ * Also, if the combination of bit time and number of pins is large, the
+ * specified cycle time may not be achieved.
+ *
  */
 
 #ifndef IO_S88_H
@@ -72,33 +75,37 @@ private:
   uint8_t *_values;  // Bit array, initialised in _begin method.
   int _currentByteIndex;
   unsigned long _lastAquireCycleStart;
+  uint16_t _pulseDelayTime;  // Delay microsecs; can be tuned for the S88 hardware
+  uint16_t _shortDelayTime;
+  uint8_t _bitIndex;
+  unsigned long _lastPulseTime;
+  uint8_t _state = 0;
 
-  // The following constants are 'finger-in-the-air' as there is no S88 spec for timing. 
-  // The propagation delays in the hardware are theoretically less than a microsecond
-  // for cable lengths up to 30 metres.  For slow hardware, the pulseDelayTime may have to be
-  // increased.  If so, then reduce bitsReadPerLoopEntry to ensure that the loop doesn't impact
-  // on the DCC signal generation.
-  const unsigned long _acquireCycleTime = 20000; // 20 milliseconds between acquire cycles
-  const uint8_t _pulseDelayTime = 2;  // Delay microsecs; can be tuned for the S88 hardware
-  const uint8_t _bitsReadPerLoopEntry = 32;  // 2 x 32 x 2us = 128us per loop entry.  Must be multiple of 8.
+
+  // The acquire cycle time is a target maximum rate.  If there are a lot of signals or the
+  // bit time is long, then the cycle time may be longer.
+  const unsigned long _acquireCycleTime = 20000; // target 20 milliseconds between acquire cycles
 
 public:
-  IO_S88(VPIN firstVpin, int nPins, uint8_t loadPin, uint8_t resetPin, uint8_t clockPin, uint8_t dataInPin) :
+  IO_S88(VPIN firstVpin, int nPins, uint8_t loadPin, uint8_t resetPin, uint8_t clockPin, uint8_t dataInPin, uint16_t bitTime) :
     IODevice(firstVpin, nPins),
     _loadPin(loadPin),
     _resetPin(resetPin),
     _clockPin(clockPin),
     _dataInPin(dataInPin),
     _currentByteIndex(0),
-    _lastAquireCycleStart(0)
+    _lastAquireCycleStart(0),
+    _pulseDelayTime((bitTime+1)/2)
   { 
     // Allocate memory for input values.
     _values = (uint8_t *)calloc((nPins+7)/8, 1);
+    _shortDelayTime = (_pulseDelayTime > 10) ? 10 : _pulseDelayTime;
+    _state = 0;
     addDevice(this);
   }
 
-  static void create(VPIN firstVpin, int nPins, uint8_t loadPin, uint8_t resetPin, uint8_t clockPin, uint8_t dataInPin) {
-    new IO_S88(firstVpin, nPins, loadPin, resetPin, clockPin, dataInPin);
+  static void create(VPIN firstVpin, int nPins, uint8_t loadPin, uint8_t resetPin, uint8_t clockPin, uint8_t dataInPin, uint16_t bitTime) {
+    new IO_S88(firstVpin, nPins, loadPin, resetPin, clockPin, dataInPin, bitTime);
   }
 
 protected:
@@ -123,61 +130,100 @@ protected:
 
   // Loop method acquires the input states from the shift register.
   // At the beginning of each acquisition cycle, instruct the bus registers to acquire the
-  // input states from the latches, then reset the latches.  On this and
+  // input states from the latches, then reset the latches.  On
   // subsequent loop entries, some of the input states are shifted from the
   // registers, until they have all been read.  Then the whole process
-  // resumes for the next acquisition cycle.
+  // resumes for the next acquisition cycle.  The operations are spread over consecutive
+  // loop entries to restrict the amount of time taken in each entry.
   void _loop(unsigned long currentMicros) override {
     // If just starting a new read, then latch the input values into the S88
-    // registers.
-    if (_currentByteIndex == 0) {
-      _lastAquireCycleStart = currentMicros;
-      // Set LOAD pin
-      ArduinoPins::fastWriteDigital(_loadPin, HIGH);
-      delayMicroseconds(_pulseDelayTime);
-      // Pulse CLOCK pin to read inputs into registers
-      ArduinoPins::fastWriteDigital(_clockPin, HIGH);
-      delayMicroseconds(_pulseDelayTime);
-      ArduinoPins::fastWriteDigital(_clockPin, LOW);
-      delayMicroseconds(_pulseDelayTime);
-      // Pulse RESET pin to clear inputs ready for next acquisition period
-      ArduinoPins::fastWriteDigital(_resetPin, HIGH);
-      delayMicroseconds(_pulseDelayTime);
-      ArduinoPins::fastWriteDigital(_resetPin, LOW);
-      delayMicroseconds(_pulseDelayTime);
-      // Clear LOAD pin.  This leaves the first bit in the DATA-IN line.
-      ArduinoPins::fastWriteDigital(_loadPin, LOW);
-      delayMicroseconds(_pulseDelayTime);
-    }
-
-    // Read each bit in turn from the shiftregister.
-    for (uint8_t bitsRead = 0; _currentByteIndex < (_nPins+7)/8  && bitsRead < _bitsReadPerLoopEntry; ) {
-      uint8_t currentByteValue = 0;
-      uint8_t mask = 1;
-      for (uint8_t bitIndex = 0; bitIndex < 8; bitIndex++) {
-        if (ArduinoPins::fastReadDigital(_dataInPin)) currentByteValue |= mask;
-        bitsRead++;
-        mask <<= 1;
-          // Clock next bit in.
+    // registers.  The active edge is the rising edge in each case, so we
+    // can use shorter delays for some transitions.
+    switch (_state) {
+      case 0: // Starting cycle.  Set up LOAD and CLOCK pins.
+        _lastAquireCycleStart = currentMicros;
+        // Set LOAD pin
+        ArduinoPins::fastWriteDigital(_loadPin, HIGH);
+        _lastPulseTime = micros();
+        pulseDelay(_shortDelayTime);
+        // Pulse CLOCK pin to read inputs into registers
         ArduinoPins::fastWriteDigital(_clockPin, HIGH);
-        delayMicroseconds(_pulseDelayTime);
-        ArduinoPins::fastWriteDigital(_clockPin, LOW);        
-        if (bitsRead < _bitsReadPerLoopEntry)
-          delayMicroseconds(_pulseDelayTime);
+        // Clear CLOCK, and set up RESET pin.
+        pulseDelay(_pulseDelayTime);
+        ArduinoPins::fastWriteDigital(_clockPin, LOW);
+        pulseDelay(_shortDelayTime);
+        // Pulse RESET pin to clear inputs ready for next acquisition period
+        ArduinoPins::fastWriteDigital(_resetPin, HIGH);
+        _state = 1;
+        delayUntil(_lastPulseTime + _pulseDelayTime);
+        return;
+      case 1: // Clear RESET and LOAD
+        ArduinoPins::fastWriteDigital(_resetPin, LOW);
+        pulseDelay(_shortDelayTime);
+        ArduinoPins::fastWriteDigital(_loadPin, LOW);
+        // Initialise variables used in reading bits.
+        _currentByteIndex = _bitIndex = 0;
+        _state = 2;
+        /* fallthrough */
+      case 2:
+        // Subsequent loop entries, read each bit in turn from the shiftregister.
+        while (true) {
+          uint8_t mask = (1 << _bitIndex);
+          bool newValue = ArduinoPins::fastReadDigital(_dataInPin);
+#ifdef DIAG_IO
+          bool oldValue = _values[_currentByteIndex] & mask;
+          if (newValue != oldValue) DIAG(F("S88 VPIN:%d Value:%d"),
+            _firstVpin+_currentByteIndex*8+_bitIndex, newValue);
+#endif
+          if (newValue)
+            _values[_currentByteIndex] |= mask;
+          else
+            _values[_currentByteIndex] &= ~mask;
+          if (++_bitIndex == 8) {
+            // Byte completed, so move to next one.
+            _currentByteIndex++;
+            _bitIndex = 0;
+          }
+
+          // Check if this cycle is complete.
+          if (_currentByteIndex*8 + _bitIndex >= _nPins) {
+            // All bits in the shift register have been read now, so 
+            // don't read again until next acquisition cycle time
+            _currentByteIndex = 0;  // Reset byte counter
+            delayUntil(_lastAquireCycleStart + _acquireCycleTime);
+            _state = 0;
+            return;
+          }
+
+          // Clock next bit in.
+          pulseDelay(_pulseDelayTime);
+          ArduinoPins::fastWriteDigital(_clockPin, HIGH);
+          pulseDelay(_pulseDelayTime);
+          ArduinoPins::fastWriteDigital(_clockPin, LOW);
+
+          // See if we've taken the time we're allowed on this entry
+          if (micros() - currentMicros >= 250) {
+            delayUntil(_lastPulseTime + _pulseDelayTime);
+            return;
+          }
       }
-      _values[_currentByteIndex++] = currentByteValue;
-    }
-    if (_currentByteIndex >= (_nPins+7)/8) {
-      // All bits in the shift register have been read now, so 
-      // don't read again until next acquisition cycle time
-      _currentByteIndex = 0;  // Reset byte counter.
-      delayUntil(_lastAquireCycleStart + _acquireCycleTime);
     }
   }
 
   void _display() override {
     DIAG(F("S88 Bus Configured on Vpins %d-%d, LOAD=%d RESET=%d CLK=%d DATAIN=%d"), 
       _firstVpin, _firstVpin+_nPins-1, _loadPin, _resetPin, _clockPin, _dataInPin);
+  }
+
+  // Helper function to delay until a minimum number of microseconds have elapsed
+  // since _lastPulseTime.
+  void pulseDelay(uint16_t duration) {
+    if (duration != 0) {
+      int delayRemaining = duration - (micros() - _lastPulseTime);
+      if (delayRemaining > 0) 
+        delayMicroseconds(delayRemaining);
+    }
+    _lastPulseTime = micros();
   }
 
 };
